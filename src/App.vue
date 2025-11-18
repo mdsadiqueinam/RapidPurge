b226219d361ee47357c7c05f5fdd587d1d6ff9ce
Md Sadique Inam <md.sadique32@gmail.com>
2025-11-18 10:50:37 +0530

    feat: update App.vue layout by removing header and adjusting class attributes for improved styling

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@ const { t } = useI18n();
 
 <template>
   <div
-    class="min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+    class="min-h-screen min-w-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
   >
-    <header class="p-4 bg-blue-600 text-white">
-      <h1 class="text-2xl font-bold">{{ t("APP_TITLE") }}</h1>
-    </header>
     <main class="p-4">
-      <!-- Main content goes here -->
+      {{ t("APP_TITLE") }}
     </main>
   </div>
 </template>
